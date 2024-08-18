c72167ca4fd0fa6c7a06a83004060ed999b52f42
Suhail Chouhan <suhail.chouhan786@gmail.com>
2024-08-18 19:21:58 +0530

    add fontawesome icons and my profile images.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,33 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
+import {
+  faAward,
+  faBuildingColumns,
+  faAnglesUp,
+  faAnglesDown,
+  faEnvelope,
+  faCircleCheck
+} from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(
+  faLinkedinIn,
+  faGithub,
+  faAward,
+  faBuildingColumns,
+  faAnglesUp,
+  faAnglesDown,
+  faEnvelope,
+  faCircleCheck
+)
+
 const app = createApp(App)
 
 app.use(router)
-
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <NavBar />
     <section id="profile">
       <div class="section__pic-container">
-        <img src="@/assets/images/profile-pic.png" alt="Suhail Chouhan Picture" />
+        <img src="@/assets/images/profile-pic-1.png" alt="Suhail Chouhan Picture" />
       </div>
       <div class="section__text">
         <p class="section__text__p1">Hello I'm</p>
@@ -15,10 +15,10 @@
         </div>
         <div class="social-container">
           <a href="https://www.linkedin.com/in/suhailchouhan" target="_blank" class="icon">
-            <img src="@/assets/images/linkedin.png" alt="My Linkedin Profile" />
+            <font-awesome-icon icon="fa-brands fa-linkedin-in" class="icon" />
           </a>
           <a href="https://github.com/suhail-chouhan" target="_blank" class="icon">
-            <img src="@/assets/images/github.png" alt="My Github Profile" />
+            <font-awesome-icon icon="fa-brands fa-github" class="icon" />
           </a>
         </div>
       </div>
@@ -30,38 +30,45 @@
       </div>
       <div class="section-container">
         <div class="section__pic-container">
-          <img src="@/assets/images/about-pic.png" alt="Profile picture" class="about-pic" />
+          <img src="@/assets/images/about-pic-1.png" alt="Profile picture" class="about-pic" />
         </div>
         <div class="about-details-container">
           <div class="about-containers">
             <div class="details-container">
-              <img src="@/assets/images/experience.png" alt="Experience icon" class="icon" />
+              <font-awesome-icon icon="fa-solid fa-award" class="icon" />
               <h3>Experience</h3>
-              <p>2+ years<br />Frontend Development</p>
+              <p>4+ years<br />Frontend Development</p>
             </div>
             <div class="details-container">
-              <img src="@/assets/images/education.png" alt="education icon" class="icon" />
+              <font-awesome-icon icon="fa-solid fa-building-columns" class="icon" />
               <h3>Education</h3>
-              <p>B.Tech Computer Science</p>
+              <p>
+                B.Tech<br />
+                Computer Science
+              </p>
             </div>
           </div>
 
           <div class="text-container">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum eveniet atque ipsa
-              aspernatur id voluptatem obcaecati itaque perferendis odio dignissimos nobis adipisci
-              aliquam iusto, consectetur fuga cumque delectus nesciunt, blanditiis iure voluptate?
-              Nemo exercitationem aliquam reiciendis. Corrupti sit vitae voluptas, laborum nulla
-              explicabo ratione accusamus veritatis. Veritatis harum reprehenderit magni.
+              With more than four years of experience as a mid-level developer, I concentrate on
+              frontend development, dedicated to building scalable and efficient web
+              applications.<br />
+              My skill set includes a strong command of core JavaScript and its frameworks,
+              particularly VueJs.<br />
+              I take pride in guiding projects from initial concept to final deployment,
+              consistently delivering high-quality results.<br />
+              I am looking for opportunities that will enable me to utilize my skills in a
+              challenging environment that encourage and makes a significant impact.
             </p>
           </div>
         </div>
       </div>
-      <a href="#experience">
-        <img src="@/assets/images/arrow.png" alt="Downward arrow" class="icon arrow" />
-      </a>
       <a href="#profile">
-        <img src="@/assets/images/arrow.png" alt="Downward arrow-up" class="icon arrow arrow-up" />
+        <font-awesome-icon icon="fa-solid fa-angles-up" class="icon arrow" />
+      </a>
+      <a href="#experience">
+        <font-awesome-icon icon="fa-solid fa-angles-down" class="icon arrow arrow-up" />
       </a>
     </section>
     <section id="experience">
@@ -71,93 +78,29 @@
       </div>
       <div class="experience-details-container">
         <div class="about-containers">
-          <div class="details-container">
-            <h2 class="experience-sub-title">Frontent Development</h2>
+          <div
+            class="details-container"
+            v-for="experience in experienceList"
+            :key="experience.title"
+          >
+            <h2 class="experience-sub-title">{{ experience.title }}</h2>
             <div class="article-container">
-              <article>
-                <img src="@/assets/images/checkmark.png" alt="Experience icon" class="icon" />
+              <article v-for="technology in experience.techList" :key="technology.name">
+                <font-awesome-icon icon="fa-solid fa-circle-check" class="icon" />
                 <div>
-                  <h3>HTML</h3>
-                  <p>Experienced</p>
-                </div>
-              </article>
-              <article>
-                <img src="@/assets/images/checkmark.png" alt="Experience icon" class="icon" />
-                <div>
-                  <h3>Css</h3>
-                  <p>Experienced</p>
-                </div>
-              </article>
-              <article>
-                <img src="@/assets/images/checkmark.png" alt="Experience icon" class="icon" />
-                <div>
-                  <h3>SASS</h3>
-                  <p>Experienced</p>
-                </div>
-              </article>
-              <article>
-                <img src="@/assets/images/checkmark.png" alt="Experience icon" class="icon" />
-                <div>
-                  <h3>JavaScript</h3>
-                  <p>Experienced</p>
-                </div>
-              </article>
-              <article>
-                <img src="@/assets/images/checkmark.png" alt="Experience icon" class="icon" />
-                <div>
-                  <h3>TypeScript</h3>
-                  <p>Experienced</p>
-                </div>
-              </article>
-              <article>
-                <img src="@/assets/images/checkmark.png" alt="Experience icon" class="icon" />
-                <div>
-                  <h3>Vuetify</h3>
-                  <p>Experienced</p>
-                </div>
-              </article>
-            </div>
-          </div>
-          <div class="details-container">
-            <h2 class="experience-sub-title">Frontent Development</h2>
-            <div class="article-container">
-              <article>
-                <img src="@/assets/images/checkmark.png" alt="Experience icon" class="icon" />
-                <div>
-                  <h3>NodeJs</h3>
-                  <p>Beginner</p>
-                </div>
-              </article>
-              <article>
-                <img src="@/assets/images/checkmark.png" alt="Experience icon" class="icon" />
-                <div>
-                  <h3>MongoDB</h3>
-                  <p>Beginner</p>
-                </div>
-              </article>
-              <article>
-                <img src="@/assets/images/checkmark.png" alt="Experience icon" class="icon" />
-                <div>
-                  <h3>Express Js</h3>
-                  <p>Beginner</p>
-                </div>
-              </article>
-              <article>
-                <img src="@/assets/images/checkmark.png" alt="Experience icon" class="icon" />
-                <div>
-                  <h3>Git</h3>
-                  <p>Experienced</p>
+                  <h3>{{ technology.name }}</h3>
+                  <p>{{ technology.proficiency }}</p>
                 </div>
               </article>
             </div>
           </div>
         </div>
       </div>
-      <a href="#projects">
-        <img src="@/assets/images/arrow.png" alt="Downward arrow" class="icon arrow" />
-      </a>
       <a href="#about">
-        <img src="@/assets/images/arrow.png" alt="Downward arrow-up" class="icon arrow arrow-up" />
+        <font-awesome-icon icon="fa-solid fa-angles-up" class="icon arrow" />
+      </a>
+      <a href="#projects">
+        <font-awesome-icon icon="fa-solid fa-angles-down" class="icon arrow arrow-up" />
       </a>
     </section>
     <section id="projects">
@@ -199,11 +142,11 @@
           </div>
         </div>
       </div>
-      <a href="#contact">
-        <img src="@/assets/images/arrow.png" alt="Downward arrow" class="icon arrow" />
-      </a>
       <a href="#experience">
-        <img src="@/assets/images/arrow.png" alt="Downward arrow-up" class="icon arrow arrow-up" />
+        <font-awesome-icon icon="fa-solid fa-angles-up" class="icon arrow" />
+      </a>
+      <a href="#contact">
+        <font-awesome-icon icon="fa-solid fa-angles-down" class="icon arrow arrow-up" />
       </a>
     </section>
     <section id="contact">
@@ -213,13 +156,13 @@
       </div>
       <div class="contact-info-upper-container">
         <div class="contact-info-container">
-          <img src="@/assets/images/email.png" alt="Email icon" class="icon contact-icon scale" />
+          <font-awesome-icon icon="fa-solid fa-envelope" class="icon small" />
           <p>
             <a href="mailto:suhailchouhandev@gmail.com">suhailchouhandev@gmail.com</a>
           </p>
         </div>
         <div class="contact-info-container">
-          <img src="@/assets/images/linkedin.png" alt="LinkedIn icon" class="icon contact-icon" />
+          <font-awesome-icon icon="fa-brands fa-linkedin-in" class="icon small" />
           <p>
             <a href="https://www.linkedin.com/in/suhailchouhan" target="_blank">LinkedIn</a>
           </p>
@@ -237,9 +180,9 @@
           </ul>
         </div>
       </nav>
-      <p>Copyright &#169; 2024 Suhail Chouhan. All Rights Reserved.</p>
+      <p>Copyright &#169; {{ new Date().getFullYear() }} Suhail Chouhan. All Rights Reserved.</p>
       <a href="#projects">
-        <img src="@/assets/images/arrow.png" alt="Downward arrow-up" class="icon arrow arrow-up" />
+        <font-awesome-icon icon="fa-solid fa-angles-up" class="icon arrow arrow-up" />
       </a>
     </footer>
   </main>
@@ -247,7 +190,68 @@
 
 <script setup>
 import NavBar from '@/components/NavBar.vue'
-
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { ref } from 'vue'
+const experienceList = ref([
+  {
+    title: 'Frontend Development',
+    techList: [
+      {
+        name: 'HTML',
+        proficiency: 'Experienced'
+      },
+      {
+        name: 'CSS',
+        proficiency: 'Experienced'
+      },
+      {
+        name: 'Javascript',
+        proficiency: 'Experienced'
+      },
+      {
+        name: 'Vue Js',
+        proficiency: 'Experienced'
+      },
+      {
+        name: 'Nuxt Js',
+        proficiency: 'Experienced'
+      },
+      {
+        name: 'SASS',
+        proficiency: 'Intermidiate'
+      },
+      {
+        name: 'TypeScript',
+        proficiency: 'Intermidiate'
+      },
+      {
+        name: 'JQuery',
+        proficiency: 'Intermidiate'
+      }
+    ]
+  },
+  {
+    title: 'Other Development Tech',
+    techList: [
+      {
+        name: 'Node Js',
+        proficiency: 'Beginner'
+      },
+      {
+        name: 'MongoDB',
+        proficiency: 'Beginner'
+      },
+      {
+        name: 'Express Js',
+        proficiency: 'Beginner'
+      },
+      {
+        name: 'Git',
+        proficiency: 'Experienced'
+      }
+    ]
+  }
+])
 function downloadResume() {
   window.open('../assets/images/resume-example.pdf', '_blank')
 }
